7908105aa287623fd86a2ec5051fba8aa34e496a
Shiivong Birla <shiivongbirla27@gmail.com>
2020-05-16 15:25:15 +0530

    Add files via upload
    Weather API for Hygieia, fetches data from an open weather data API.

---
+++ WeatherAPI_Widget.R
@@ -0,0 +1,246 @@
+# Initializing all packages
+
+library(shinydashboard)
+library(dplyr)
+library(shiny)
+library(shinyalert)
+library(owmr)
+library(ggplot2)
+library(lubridate)
+library(ggdark)
+library(plotly)
+library(tidyr)
+library(shinyjs)
+
+# UI side design
+
+ui <- dashboardPage(skin = "blue", # Giving a blue header
+                    
+                    dashboardHeader(title = "Weather Forecast" # Dashboard title
+                                    
+                    ),
+                    
+                    dashboardSidebar(), # Initializing a sidebar
+                    
+                    dashboardBody( useShinyjs(), useShinyalert(), # Shiny alert for cool popups
+                      
+                      tags$head(tags$style(HTML('                       
+                                                .main-header .logo {
+                                                font-family: "Georgia", Times,
+                                                "Times New Roman",
+                                                font-weight: bold;
+                                                font-size: 24px;
+                                                font-style: italic;
+                                                }
+                                                '))), # A CSS template for the font size and font
+                      
+                      fluidRow(
+                        
+                        tabBox(height = "900px", width = "1000px",
+                               
+                               tabPanel(title = tagList(icon("project-diagram", class = "fas fa-project-diagram") # Creating the tabset panels
+                                                        
+                                                        , "FORECAST FOR THE NEXT 5 DAYS"),
+                                        
+                                        box(plotOutput("TempPlot"), status = "primary", solidHeader = TRUE, # Temperature plot
+                                            
+                                            title = "Temp VS Feels Like", width = 8, height = 512, collapsible = TRUE),
+                                        
+                                        box(tableOutput("CurrTemp"), status = "primary", # Small boxes to show current weather attributes
+                                            
+                                            title = "Current Temperature (C)", width = 3, height = 85, collapsible = TRUE),
+                                        
+                                        box(tableOutput("CurrFeelsLike"), status = "primary", # Small boxes to show current weather attributes
+                                            
+                                            title = "Feels Like Currently (C)", width = 3, height = 85,
+                                            
+                                            collapsible = TRUE),
+                                        
+                                        box(tableOutput("CurrHumidity"), status = "primary", # Small boxes to show current weather attributes
+                                            
+                                            title = "Current Humidity (%)", width = 3, height = 85, collapsible = TRUE),
+                                        
+                                        box(tableOutput("CurrWind"), status = "primary", # Small boxes to show current weather attributes
+                                            
+                                            title = "Current Wind Speed (km/h)", width = 3, height = 85, collapsible = TRUE),
+                                        
+                                        box(tableOutput("CurrWeatherDesc"), status = "primary", # Small boxes to show current weather attributes
+                                            
+                                            title = "Current Weather Description", width = 3, height = 85, collapsible = TRUE),
+                                        
+                                        box(tableOutput("CurrPressure"), status = "primary", # Small boxes to show current weather attributes
+                                            
+                                            title = "Current Pressure (Pascal)", width = 3, height = 85, collapsible = TRUE)
+                               ),
+                               
+                               
+                               tabPanel(title = tagList(icon("th-list", lib = "glyphicon"), "3-h 5-day Forecast"),
+                                        
+                                        DT::dataTableOutput("ForecastTable") # Data table for the 3-h 5-day forecast
+                               ),
+                               
+                               tabPanel(title = tagList(icon("Wind", class = "fas fa-wind"), "Wind Speed Forecast"), # Another tabset panel for wind
+                                        
+                                        box(plotOutput("Wind"), status = "primary", solidHeader = TRUE,
+                                            
+                                            title = "WIND", width = 8, height = 512, collapsible = TRUE)
+                               ),
+                               
+                               tabPanel(title = tagList(icon("Wind", class = "fas fa-wind"), "Humidity Forecast"), # Another tabset panel for humidity
+                                        
+                                        box(plotOutput("Humidity"), status = "primary", solidHeader = TRUE,
+                                            
+                                            title = "HUMIDITY", width = 8, height = 512, collapsible = TRUE)
+                               ),
+                               
+                               tabPanel(title = tagList(icon("Wind", class = "fas fa-wind"), "Pressure Forecast"), # Another tabset panel for pressure
+                                        
+                                        box(plotOutput("Pressure"), status = "primary", solidHeader = TRUE,
+                                            
+                                            title = "PRESSURE", width = 8, height = 512, collapsible = TRUE)
+                               )
+                               
+                        )
+                      )
+                      )) 
+
+# Server side design
+
+server <- function(input, output) {
+  
+  addClass(selector = "body", class = "sidebar-collapse") # Adding a JS class to automatically collapse the sidebar
+  
+  shinyalert(title = "WELCOME TO OUR WEATHER API!", type = "info", showConfirmButton = TRUE, confirmButtonText = "GOT IT!", 
+             text = "You will now be able to receive warnings about 
+             major temperature fluctuations and preventive measures!") # A pop-up alert to let the user know what the application is about
+  
+  owmr_settings("ece333c8c4fcf1e8b62947374d3c34f5") # Setting API Access token
+  
+  Sys.setenv(OWM_API_KEY = "ece333c8c4fcf1e8b62947374d3c34f5") # Storing Access token in R environment
+  
+  (weather <- get_current(2158177, units = "metric") %>%
+      owmr_as_tibble()) %>% names() # Fetching current weather data
+  
+  melbs <- get_forecast(2158177, units = "metric") %>% owmr_as_tibble() # Fetching forecasted weather data fro the next 5 days 
+  
+  melbs_forecast <- melbs %>% mutate(date = lubridate::date(dt_txt)) %>% group_by(date) %>% 
+    summarise(temp_avg = mean(temp), feels_like_avg = mean(feels_like), humidity_avg = mean(humidity), pressure_avg = mean(pressure),
+              wind_speed_avg = mean(wind_speed)) # Creating a dataframe with average temperatures, humidity, pressure etc.
+  
+  
+  output$TempPlot <- renderPlot({
+    
+    melbs_forecast_tidy <- melbs_forecast %>% select(date, temp_avg, feels_like_avg) %>% 
+      gather(key = "Temperature", value = "value", -date)
+    
+    melbs_forecast_tidy_plot <- melbs_forecast_tidy %>% ggplot(aes(x = date, y = value, label = round(value, 2))) + 
+                                geom_line(aes(color = Temperature, linetype = Temperature)) + 
+                                scale_color_manual(values = c("blue", "red")) + 
+                                dark_theme_gray(base_family = "Fira Sans Condensed Light", base_size = 14) + 
+                                geom_point(col = "#E4F00A") + 
+                                geom_text(hjust = 0.3, vjust = 1.3) +
+                                ggtitle("Avg. Temperature vs Avg. Feels Like over the next 5 days") +
+                                xlab("Date") + ylab("Temperature in Degree Celsius") # side-by-side line chart for the forecasted temperature vs the forecasted feels like over the next 5 days
+    
+    melbs_forecast_tidy_plot # Plotting the ggplot object
+    
+  })
+  
+  output$CurrHumidity <- renderText({
+    
+    weather$humidity # Returning current humidity
+    
+  })
+  
+  
+  output$ForecastTable <- DT::renderDataTable({
+    
+    melbs_selected <- melbs %>% select(c("dt_txt", "temp", "pressure", "humidity", "weather_description", "wind_speed", "feels_like"))
+    
+    names(melbs_selected) <- c("Datetime", "Temperature", "Pressure", "Humidity", "Weather Description", "Wind Speed", "Feels Like") # Changing default dataframe columns names
+  
+    melbs_selected # Returning the dataframe
+    
+  })
+  
+  output$CurrTemp <- renderText({
+    
+    weather$temp # Returning current temperature
+    
+  })
+  
+  output$CurrPressure <- renderText({
+    
+    weather$pressure # Returning current pressure
+    
+  })
+  
+  output$CurrWind <- renderText({
+    
+    weather$wind_speed # Returning current wind speed
+    
+  })
+  
+  output$CurrFeelsLike <- renderText({
+    
+    weather$feels_like # Returning current feels like temperature
+  
+  })
+  
+  output$CurrWeatherDesc <- renderText({
+    
+    toupper(weather$weather_description) # Returning current weather description in upper case
+    
+  })
+  
+  output$Wind <- renderPlot({
+    
+      melbs_forecast_tidy <- melbs_forecast %>% select(date, wind_speed_avg) %>% 
+                             gather(key = "Wind Speed", value = "value", -date) # Transforming wind speed data to plot as a line chart
+    
+      melbs_forecast_tidy %>% ggplot(aes(x = date, y = value, label = round(value, 2))) + 
+      geom_line(aes(color = 'Wind Speed')) + 
+      scale_color_manual(values = c("orange")) + 
+      dark_theme_gray(base_family = "Fira Sans Condensed Light", base_size = 14) + 
+      geom_point(col = "#E4F00A") + 
+      geom_text(hjust = 0.3, vjust = 1.3) +
+      ggtitle("Avg. Wind Speed over the next 5 days") +
+      xlab("Date") + ylab("Wind Speed in km/h") # ggplot implementation for the line chart with a dark theme, line and points
+    
+  })
+  
+  output$Humidity <- renderPlot({
+    
+      melbs_forecast_tidy <- melbs_forecast %>% select(date, humidity_avg) %>% 
+      gather(key = "Humidity", value = "value", -date) # Transforming humidity data to plot as a line chart
+    
+      melbs_forecast_tidy %>% ggplot(aes(x = date, y = value, label = round(value, 2))) + 
+      geom_line(aes(color = 'Humidity')) + 
+      scale_color_manual(values = c("red")) + 
+      dark_theme_gray(base_family = "Fira Sans Condensed Light", base_size = 14) + 
+      geom_point(col = "#E4F00A") + 
+      geom_text(hjust = 0.3, vjust = 1.3) +
+      ggtitle("Avg. Humidity over the next 5 days") +
+      xlab("Date") + ylab("Humidity in %") # ggplot implementation for the line chart with a dark theme, line and points
+    
+  })
+  
+  output$Pressure <- renderPlot({
+    
+      melbs_forecast_tidy <- melbs_forecast %>% select(date, pressure_avg) %>% 
+      gather(key = "Pressure", value = "value", -date) # Transforming pressure data to plot as a line chart
+    
+      melbs_forecast_tidy %>% ggplot(aes(x = date, y = value, label = round(value, 2))) + 
+      geom_line(aes(color = 'Pressure')) + 
+      scale_color_manual(values = c("blue")) + 
+      dark_theme_gray(base_family = "Fira Sans Condensed Light", base_size = 14) + 
+      geom_point(col = "#E4F00A") + 
+      geom_text(hjust = 0.3, vjust = 1.3) +
+      ggtitle("Avg. Pressure over the next 5 days") +
+      xlab("Date") + ylab("Pressure in Pascals") # ggplot implementation for the line chart with a dark theme, line and points
+    
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
